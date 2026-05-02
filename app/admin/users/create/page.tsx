b465fa97ee4874a888bedd4d7cd567a708360869
Peter Heile <peterjheile@gmail.com>
2026-05-02 14:41:31 -0400

    [WIP] Clean Staging Branch for Complete Overhaul
    Needed to a clean start for staging with the
    new overhaul. Plans include changing EVERYTHING,
    so this is a simple and effective solution.

--- app/admin/users/create/page.tsx
+++
@@ -1,8 +0,0 @@
-import { requireSuperAdmin } from "@/lib/auth"
-import { AdminCreateForm } from "./AdminCreateForm"
-
-export default async function CreateAdminPage() {
-  await requireSuperAdmin()
-
-  return <AdminCreateForm />
-}
